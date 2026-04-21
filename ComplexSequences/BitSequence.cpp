d0f6ffd45b85a0e8b147dcb0e600170cc7334370
K01N1323 <115662036+K01N1323@users.noreply.github.com>
2026-04-21 12:20:00 +0300

    Исправлены ошибки в BitSqeuence

--- ComplexSequences/BitSequence.cpp
+++ ComplexSequences/BitSequence.cpp
@@ -1,99 +1,89 @@
-#include <iostream>
+#include "BitSequence.h"
 
-#include "../Sequences/MutableArraySequence.h"
+Bit::Bit(bool v) : value(v) {}
 
-class Bit {
-private:
-    bool value;
+Bit::Bit(int v) : value(v != 0) {}
 
-public:
-    Bit(bool v = false) : value(v) {}
-    
-    Bit(int v) : value(v != 0) {}
+bool Bit::GetValue() const { 
+    return value; 
+}
 
-    bool GetValue() const { 
-        return value; 
+Bit Bit::operator&(const Bit &other) const {
+    return Bit(this->value && other.value);
+}
+
+Bit Bit::operator|(const Bit &other) const {
+    return Bit(this->value || other.value);
+}
+
+Bit Bit::operator^(const Bit &other) const {
+    return Bit(this->value != other.value);
+}
+
+Bit Bit::operator~() const { 
+    return Bit(!this->value); 
+}
+
+bool Bit::operator==(const Bit &other) const { 
+    return this->value == other.value; 
+}
+
+std::ostream &operator<<(std::ostream &os, const Bit &b) { 
+    os << b.value;
+    return os;
+}
+
+
+BitSequence::BitSequence() : MutableArraySequence<Bit>() {}
+
+BitSequence *BitSequence::BitAnd(Sequence<Bit> *other) const {
+    BitSequence *NewBitSequence = new BitSequence();
+
+    int ElementsToChange = (this->GetLength() < other->GetLength())
+                                 ? this->GetLength()
+                                 : other->GetLength();
+
+    for (int index = 0; index < ElementsToChange; index++) {
+        NewBitSequence->append(this->get(index) & other->get(index));
     }
 
-    Bit operator&(const Bit &other) const {
-        return Bit(this->value && other.value);
-    }
-    
-    Bit operator|(const Bit &other) const {
-        return Bit(this->value || other.value);
-    }
-    
-    Bit operator^(const Bit &other) const {
-        return Bit(this->value != other.value);
-    }
-    
-    Bit operator~() const { 
-        return Bit(!this->value); 
+    return NewBitSequence;
+}
+
+BitSequence *BitSequence::BitOr(Sequence<Bit> *other) const {
+    BitSequence *NewBitSequence = new BitSequence();
+
+    int ElementsToChange = (this->GetLength() < other->GetLength())
+                                 ? this->GetLength()
+                                 : other->GetLength();
+
+    for (int index = 0; index < ElementsToChange; index++) {
+        NewBitSequence->append(this->get(index) | other->get(index));
     }
 
-    bool operator==(const Bit &other) const { 
-        return this->value == other.value; 
+    return NewBitSequence;
+}
+
+BitSequence *BitSequence::BitXor(Sequence<Bit> *other) const {
+    BitSequence *NewBitSequence = new BitSequence();
+
+    int ElementsToChange = (this->GetLength() < other->GetLength())
+                                 ? this->GetLength()
+                                 : other->GetLength();
+
+    for (int index = 0; index < ElementsToChange; index++) {
+        NewBitSequence->append(this->get(index) ^ other->get(index));
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const Bit &b) { 
-        os << b.value;
-        return os;
+    return NewBitSequence;
+}
+
+BitSequence *BitSequence::BitNot() const {
+    BitSequence *NewBitSequence = new BitSequence();
+
+    for (int index = 0; index < this->GetLength(); index++) {
+        NewBitSequence->append(~this->get(index));
     }
-};
 
-class BitSequence : public MutableArraySequence<Bit> {
-public:
-    BitSequence() : MutableArraySequence<Bit>() {}
-
-    BitSequence *BitAnd(Sequence<Bit> *other) const {
-        BitSequence *NewBitSequence = new BitSequence();
-
-        int ElementsToChange = (this->GetLength() < other->GetLength())
-                                     ? this->GetLength()
-                                     : other->GetLength();
-
-        for (int index = 0; index < ElementsToChange; index++) {
-            NewBitSequence->append(this->get(index) & other->get(index));
-        }
-
-        return NewBitSequence;
-    }
-    
-    BitSequence *BitOr(Sequence<Bit> *other) const {
-        BitSequence *NewBitSequence = new BitSequence();
-
-        int ElementsToChange = (this->GetLength() < other->GetLength())
-                                     ? this->GetLength()
-                                     : other->GetLength();
-
-        for (int index = 0; index < ElementsToChange; index++) {
-            NewBitSequence->append(this->get(index) | other->get(index));
-        }
-
-        return NewBitSequence;
-    }
-    
-    BitSequence *BitXor(Sequence<Bit> *other) const {
-        BitSequence *NewBitSequence = new BitSequence();
-
-        int ElementsToChange = (this->GetLength() < other->GetLength())
-                                     ? this->GetLength()
-                                     : other->GetLength();
-
-        for (int index = 0; index < ElementsToChange; index++) {
-            NewBitSequence->append(this->get(index) ^ other->get(index));
-        }
-
-        return NewBitSequence;
-    }
-    
-    BitSequence *BitNot() const {
-        BitSequence *NewBitSequence = new BitSequence();
-
-        for (int index = 0; index < this->GetLength(); index++) {
-            NewBitSequence->append(~this->get(index));
-        }
-
-        return NewBitSequence;
-    }
-};
+    return NewBitSequence;
+}
--- ComplexSequences/BitSequence.h
+++ ComplexSequences/BitSequence.h
@@ -1,4 +1,36 @@
-#ifndef BIT_SQEUENCE_H
-#define BIT_SQEUENCE_H
+#ifndef BIT_SEQUENCE_H
+#define BIT_SEQUENCE_H
 
-#endif // BIT_SQEUENCE_H
+#include <iostream>
+#include "../Sequences/MutableArraySequence.h"
+
+class Bit {
+private:
+    bool value;
+
+public:
+    Bit(bool v = false);
+    Bit(int v);
+
+    bool GetValue() const;
+
+    Bit operator&(const Bit &other) const;
+    Bit operator|(const Bit &other) const;
+    Bit operator^(const Bit &other) const;
+    Bit operator~() const;
+    bool operator==(const Bit &other) const;
+
+    friend std::ostream &operator<<(std::ostream &os, const Bit &b);
+};
+
+class BitSequence : public MutableArraySequence<Bit> {
+public:
+    BitSequence();
+
+    BitSequence *BitAnd(Sequence<Bit> *other) const;
+    BitSequence *BitOr(Sequence<Bit> *other) const;
+    BitSequence *BitXor(Sequence<Bit> *other) const;
+    BitSequence *BitNot() const;
+};
+
+#endif // BIT_SEQUENCE_H
